b3580ea0c8f04d8cb82e4518518637767371ce14
GaviniDivyaMadhuri <gavinimadhuri13@gmail.com>
2026-02-20 19:08:39 +0530

    Update database password in db.js

--- Database/db.js
+++ Database/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Admin@1234',
+  password: 'Admin@12',
   database: 'edusafeguard',
   waitForConnections: true,
   connectionLimit: 10,
